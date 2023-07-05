d2edeeb538757155c94ad5989887135434ebfbf5
Abdul200218 <122101031+Abdul200218@users.noreply.github.com>
2023-07-05 08:08:03 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql');
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -8,4 +10,31 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+// Login route
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Validate username and password
+  if (username === 'admin' && password === 'password') {
+    // Credentials are valid, create session or generate JWT here
+
+    // Redirect to home page
+    res.redirect('/home');
+  } else {
+    // Invalid credentials, handle error
+    res.status(401).send('Invalid username or password');
+  }
+});
+
+// Other routes
+app.get('/home', (req, res) => {
+  // Handle home page rendering
+  res.send('Welcome to the home page');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
